767cc1045d0b96a7db75fd12812f058b1a5185ad
tarekHaker <tarekbnzaied@gmail.com>
2023-11-12 00:15:01 +0400

    update3.0

--- front-end/src/component/icon.js
+++ front-end/src/component/icon.js
@@ -5,11 +5,13 @@ import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import NavigationIcon from '@mui/icons-material/Navigation';
+import { useNavigate } from 'react-router-dom'
 
-export default function FloatingActionButtons() {
+export default function FloatingActionButtons({ seize }) {
+  const navigate = useNavigate();
   return (
-    <Box sx={{ '& > :not(style)': { m: 1 },marginLeft:'50%',padding:'20px'}}>
-      <Fab color="primary" aria-label="add">
+    <Box sx={{ '& > :not(style)': { m: 1 },marginLeft:'50%',padding:'20px'}} onClick={()=>{navigate('/form')}}>
+      <Fab color="primary" aria-label="add" size= {seize }>
         <AddIcon />
       </Fab>
     
