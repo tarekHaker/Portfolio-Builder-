e4a2845b174515684279272af4e3e1901e90ffb2
tarekHaker <tarekbnzaied@gmail.com>
2023-11-12 22:36:05 +0400

    update4.0

--- front-end/src/component/pages/form.js
+++ front-end/src/component/pages/form.js
@@ -4,11 +4,18 @@ import IconButton from '@mui/material/IconButton';
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 import InputAdornment from '@mui/material/InputAdornment';
 import TextField from '@mui/material/TextField';
-import FloatingActionButtons from '../icon';
+import FloatingActionButtons from '../icon'; 
+import InputFileUpload from '../uploadButton'; 
+
+const ImagePreview = ({ image }) => {
+  if (!image) return null;
+  return <img src={URL.createObjectURL(image)} alt="Uploaded" style={{ width: '20%', height: 'auto', marginTop: '20px' }} />;
+};
 
 export default function Form() {
   const [educationFields, setEducationFields] = useState(['Education ']);
   const [workFields, setWorkFields] = useState(['Work Experience ']);
+  const [uploadedImage, setUploadedImage] = useState(null);
 
   const addEducationField = () => {
     setEducationFields([...educationFields, `Education ${educationFields.length + 1}`]);
@@ -18,87 +25,83 @@ export default function Form() {
     setWorkFields([...workFields, `Work Experience ${workFields.length + 1}`]);
   };
 
+  const handleFileChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      setUploadedImage(event.target.files[0]);
+    }
+  };
+
+  const handleSubmit = () => {
+    const formData = new FormData();
+    if (uploadedImage) {
+      formData.append('image', uploadedImage);
+    }
+   
+    console.log('Form Submitted', formData); 
+  };
+
   return (
     <>
-    
-    <Box
-      component="form"
-      sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
-      noValidate
-      autoComplete="off"
-      
-      style={{ display:'flex', justifyContent:'center', alignItems:'center' , paddingTop:'5vh',
-      paddingBottom:'15vh', backgroundColor:'#e1e1db'
-      }}
-    >
-   
-      <div style={{ display: 'flex', flexDirection: 'column', width: '70%',
-      backgroundColor:'white',padding:'3vh 6vh', borderRadius:'15px'
-       }}>
-        {/* Name and Last Name Fields */}
-        <TextField
-          label="Name"
-          type="search"
-          variant="standard"
-          style={{ width: '100%', marginBottom: '20px' }}
-        />
-        <TextField
-          label="Last name"
-          type="search"
-          variant="standard"
-          style={{ width: '100%', marginBottom: '20px' }}
-        />
+      <Box
+        component="form"
+        sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
+        noValidate
+        autoComplete="off"
+        style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '5vh', paddingBottom: '15vh', backgroundColor: '#e1e1db' }}
+      >
+        <div style={{ display: 'flex', flexDirection: 'column', width: '70%', backgroundColor: 'white', padding: '3vh 6vh', borderRadius: '15px' }}>
+          <TextField label="Name" type="search" variant="standard" style={{ width: '100%', marginBottom: '20px' }} />
+          <TextField label="Last name" type="search" variant="standard" style={{ width: '100%', marginBottom: '20px' }} />
 
-        {/* Dynamic Education Fields */}
-        {educationFields.map((label, index) => (
-          <TextField
-            key={index}
-            label={label}
-            type="search"
-            variant="standard"
-            style={{ width: '100%', marginBottom: '20px' }}
-            InputProps={{
-              endAdornment: index === educationFields.length - 1 && (
-                <InputAdornment position="end">
-                  <IconButton onClick={addEducationField}>
-                    <AddCircleOutlineOutlinedIcon />
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
-        ))}
+          {educationFields.map((label, index) => (
+            <TextField
+              key={index}
+              label={label}
+              type="search"
+              variant="standard"
+              style={{ width: '100%', marginBottom: '20px' }}
+              InputProps={{
+                endAdornment: index === educationFields.length - 1 && (
+                  <InputAdornment position="end">
+                    <IconButton onClick={addEducationField}>
+                      <AddCircleOutlineOutlinedIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          ))}
 
-        {/* Dynamic Work Experience Fields */}
-        {workFields.map((label, index) => (
-          <TextField
-            key={index}
-            label={label}
-            type="search"
-            variant="standard"
-            style={{ width: '100%', marginBottom: '20px' }}
-            InputProps={{
-              endAdornment: index === workFields.length - 1 && (
-                <InputAdornment position="end">
-                  <IconButton onClick={addWorkField}>
-                    <AddCircleOutlineOutlinedIcon />
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-          />
-        ))}
+          {workFields.map((label, index) => (
+            <TextField
+              key={index}
+              label={label}
+              type="search"
+              variant="standard"
+              style={{ width: '100%', marginBottom: '20px' }}
+              InputProps={{
+                endAdornment: index === workFields.length - 1 && (
+                  <InputAdornment position="end">
+                    <IconButton onClick={addWorkField}>
+                      <AddCircleOutlineOutlinedIcon />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          ))}
 
-        {/* About You Field */}
-        <TextField
-          label="About you"
-          type="search"
-          variant="standard"
-          style={{ width: '100%', marginBottom: '20px' }}
-        />
-        <FloatingActionButtons seize={'small'}/>
-      </div>
-    </Box>
+          <TextField label="About you" type="search" variant="standard" style={{ width: '100%', marginBottom: '20px' }} />
+
+          <div style={{ width: '30%', marginTop: '5vh' }}>
+            <InputFileUpload onChange={handleFileChange} />
+          </div>
+
+          <ImagePreview image={uploadedImage}  />
+
+          <FloatingActionButtons onClick={handleSubmit} seize={'small'} /> 
+        </div>
+      </Box>
     </>
   );
 }
--- front-end/src/component/pages/homePage.js
+++ front-end/src/component/pages/homePage.js
@@ -5,7 +5,12 @@ export default function HomePage() {
   return (
     <div>
       <Nav />
-      <div className='background'></div>
+      <div className='background'>
+        <div className='presentation-text'>
+        <h1 className='creative-portfolio'>CREATIVE </h1>
+        <h1 className='creative-portfolio'> PORTFOLIO</h1>
+        </div>
+      </div>
     </div>
   )
 }
