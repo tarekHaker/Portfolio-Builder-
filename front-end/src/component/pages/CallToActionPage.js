2892e3fb7767536ccd00c9b60824ee3b2a430637
AnisJemail <anis@polyglotanis@polyglot>
2023-11-13 13:09:39 +0100

    commit the CallToAction page

---
+++ front-end/src/component/pages/CallToActionPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CallToActionPage.css";
+
+const CallToActionPage = () => {
+  return <div className="call-to-action-page"></div>;
+};
+
+export default CallToActionPage;
