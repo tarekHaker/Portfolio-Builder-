184017942fc75bd130d779d9d80d51db34830ad3
ikramby <ikram@polyglot-digital.com>
2023-11-16 11:48:53 +0100

    fix bug

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ const app = express();
 
 const mongoose = require('mongoose'); 
 
+mongoose.connect('mongodb://127.0.0.1:27017/Portfolio-Builder-', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
