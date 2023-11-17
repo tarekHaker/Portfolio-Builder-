54612c7889ae859ecaa25703e7213fed9891174f
ikramby <ikram@polyglot-digital.com>
2023-11-17 09:50:23 +0100

    fix bug

--- backend/routes/portfolios.js
+++ backend/routes/portfolios.js
@@ -5,22 +5,21 @@ const crypto = require('crypto');
 const PortfolioController = require('../controllers/PortfolioController');
 const Portfolio = require('../models/Portfolio');
 
-const generateFileName = (bytes = 32) =>
+  const generateFileName = (bytes = 32) =>
   crypto.randomBytes(bytes).toString('hex');
-  const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, '../uploads/'));
-    },
-    filename: function (req, file, cb) {
-      const originalExtension = path.extname(file.originalname);
-      const uniqueSuffix = generateFileName();
-      cb(null, uniqueSuffix + originalExtension);
-    },
-  });
-  
- 
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'routes/uploads/');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = generateFileName();
+    cb(null, uniqueSuffix + '.jpg');
+  },
+});
 
 const upload = multer({ storage: storage });
+
 router.get('/portfolios/:id', PortfolioController.getPortfolioByUserId);
 
 router.get('/portfolios/count/:id', PortfolioController.countPortfoliosByUserId);
